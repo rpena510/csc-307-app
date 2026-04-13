a89a673ac80c1419b1b909687ac6aa0c81e1c9b2
Rafael <rpena06@calpoly.edu>
2026-04-13 16:21:16 -0700

    commit add users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,12 @@ const findUserByName = (name) => {
 const findUserById = (id) => 
     users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+}
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -63,6 +69,12 @@ app.get("/users/:id", (req, res) => {
     } else {
         res.send(result);
     }
+});
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
 });
 
 app.listen(port, () => {
