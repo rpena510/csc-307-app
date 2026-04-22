cbeb98233651a251a896920114679c2d5f741e9b
Rafael <rpena06@calpoly.edu>
2026-04-21 22:35:22 -0700

    CORS

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -61,7 +62,7 @@ const deleteUserById = (id) => {
     }
 }
     
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
